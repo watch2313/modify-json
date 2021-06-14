93e8b6ee94b1336c2bbf983813c49de0efb209ac
mayane26 <73557407+mayane26@users.noreply.github.com>
2021-06-14 10:38:57 +0200

    Update updatejson.sh

--- updatejson.sh
+++ updatejson.sh
@@ -84,6 +84,7 @@ for _filename in $(ls *.json); do
                                #We loop on the variables of the tree in question
                           for key in $currentTreeNodes; do
 
+                               #Retrieves the secret value of the attribute
                                currentkey=${!key}
                                
                                #Cut the CONF2_${currentTreeName}__${node}__ part to access the attributes directly
