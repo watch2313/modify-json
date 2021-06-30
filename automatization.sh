ad26fdcf3dc67d337548c1f8d7fd1b8c604d8083
mayane26 <73557407+mayane26@users.noreply.github.com>
2021-06-30 17:44:56 +0200

    Update automatization.sh

--- automatization.sh
+++ automatization.sh
@@ -13,9 +13,9 @@ function parse_yaml {
       indent = length($1)/2;
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+        if (length($3) > 0) {
+          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+          printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
       }
    }'
 }
