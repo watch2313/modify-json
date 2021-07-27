82e7752860562367aeeda0506f6b35af26af202d
mayane26 <73557407+mayane26@users.noreply.github.com>
2021-07-27 11:00:22 +0200

    Update getSecretValue.sh

--- getSecretValue.sh
+++ getSecretValue.sh
@@ -5,6 +5,9 @@ FILE=$1
 ENV=$2
 KEY=$3
 
+#We need the parse_yaml.sh script wich contains the function that parses the yaml files
+source /path/to/parse_yaml.sh
+
 #2) check that there is the right number of arguments
 if [ $# -ne 3 ]; then
      echo "Incorrect number of arguments, syntax $0 "FILE" "ENV" "KEY""
@@ -17,24 +20,6 @@ fi
    fi
 
 #4) Reading the yaml file
-
-function parse_yaml {
-     local prefix=$2
-     local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-     sed -ne "s|^\($s\):|\1|"  \
-          -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-          -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
-      awk -F$fs '{
-        indent = length($1)/2;
-        vname[indent] = $2;
-        for (i in vname) {if (i > indent) {delete vname[i]}}
-            if (length($3) > 0) {
-              vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-              printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-         }
-     }'
- }
-
 eval "$(parse_yaml $FILE "CONF1_")"
 
 #5) retrieve of the value of ENV.KEY
