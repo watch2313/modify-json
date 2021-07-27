b74b34ce622bfc46adb5ff7bed371a46f24219ef
mayane26 <73557407+mayane26@users.noreply.github.com>
2021-07-27 11:01:57 +0200

    Update automatization.sh

--- automatization.sh
+++ automatization.sh
@@ -3,25 +3,11 @@
 ENV=$1
 FUNCTION=$2
 
+#We need the parse_yaml.sh script wich contains the function that parses the yaml files
+source /path/to/parse_yaml.sh
+
 #Retrieving variables from the yaml file
-function parse_yaml {
-   local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-   sed -ne "s|^\($s\):|\1|" \
-        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
-        if (length($3) > 0) {
-          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-          printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
-      }
-   }'
-}
-
-
+#Read the yaml file, the function is in the parse_yaml.sh script
 eval "$(parse_yaml /path/to/conf1.yaml "CONF_")"
 
 
